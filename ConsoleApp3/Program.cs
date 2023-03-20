e7834b4d8f5f4b63a96dfb91819c2c3aadac1d5a
WideBirb <126762178+WideBirb@users.noreply.github.com>
2023-03-20 18:03:50 +0800

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -13,8 +13,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
             int Attempts = 0;
 
             for (int current_col = 0; current_col < Board.GetLength(0); current_col++)
-            {
-
                 for (int current_row = 0; current_row < Board.GetLength(0); current_row++)
                 {
 
@@ -42,14 +40,12 @@ namespace MyApp // Note: actual namespace depends on the project name.
                             for (int y = 0; y < Board.GetLength(1); y++)
                                 Board[x, y] = 0;
                     }
-
                     else
                         Board[current_col, current_row] = Pool[rnd.Next(0, Pool.Count)];
 
                     Pool.Clear();
 
                 }
-            }
 
             Console.WriteLine("ATTEMPTS :" + Attempts);
             for (int i = 0; i < Board.GetLength(0); i++)
@@ -60,7 +56,6 @@ namespace MyApp // Note: actual namespace depends on the project name.
                         Console.Write("- ");
                 }
                 Console.WriteLine();
-
                 for (int j = 0; j < Board.GetLength(1); j++)
                 {
                     if ((j % 3 == 0) && !(j == 0))
@@ -68,9 +63,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
                     Console.Write(Board[i, j] + "\t");
                 }
                 Console.WriteLine();
-
             }
-
         }
     }
 }
